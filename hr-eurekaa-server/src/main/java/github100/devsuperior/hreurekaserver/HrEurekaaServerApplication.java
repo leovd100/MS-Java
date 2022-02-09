e808f51e95cf097d11468343edf520215936cf76
leonardo <demetrio.vd@hotmail.com>
2022-02-09 20:13:19 -0300

    Project hr-eureka-server created

---
+++ hr-eurekaa-server/src/main/java/github100/devsuperior/hreurekaserver/HrEurekaaServerApplication.java
@@ -0,0 +1,13 @@
+package github100.devsuperior.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaaServerApplication.class, args);
+	}
+
+}
---
+++ hr-eurekaa-server/src/test/java/github100/devsuperior/hreurekaserver/HrEurekaaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package github100.devsuperior.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
